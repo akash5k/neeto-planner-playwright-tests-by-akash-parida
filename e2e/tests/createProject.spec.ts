0a70eec00c1ca38bc784c84e2c9d4bbcf1953fd8
akash5k <akashparida185@gmail.com>
2024-04-23 00:40:11 +0530

    update for project pom

--- e2e/tests/createProject.spec.ts
+++ e2e/tests/createProject.spec.ts
@@ -1,4 +1,4 @@
-import {test ,expect} from "@playwright/test"
+import {test} from "../fixtures"
 import { faker } from "@faker-js/faker"
 
 test.describe("Create task",()=>{
@@ -8,16 +8,11 @@ test.describe("Create task",()=>{
         projectName = faker.word.words({count:2});
         projectDescription = faker.word.words({count:10});
     })
-    test("Should create a task and verify on project details page",async ({page})=>{
+    test("Should create a task and verify on project details page",async ({
+        page,
+    projectPage})=>{
         await page.goto("/");
-        await page.getByRole('button', { name: 'Add new project' }).click();
-        await page.getByPlaceholder('Enter project name').fill(projectName);
-        await page.getByPlaceholder('Enter description').fill(projectDescription);
-        await page.getByRole('button', { name: 'Save changes' }).click();
-
-        //verify project creation
-        await expect(page.getByText(projectName)).toBeVisible();
-        // await expect(page.getByTestId('neeto-molecules-header')).toContainText(projectName);
+        await projectPage.addProject({projectName,projectDescription});
 
     })
 })
