e637057bf2b8d767da0b0f671bd8d5d164b2384b
akash5k <akashparida185@gmail.com>
2024-04-22 11:07:39 +0530

    add test file for login

---
+++ e2e/login.spec.ts
@@ -0,0 +1,22 @@
+//login.spec.ts
+
+import { faker } from "@faker-js/faker";
+import { test, expect } from "@playwright/test"
+
+test.describe("Login Page", () => {
+    let loginCode: string;
+    test.beforeEach(() => {
+        loginCode = faker.random.numeric(6);
+    })
+    test("Should login the user correctly", async ({ page }) => {
+        // test.slow();
+        // test.setTimeout(1200000);
+        await page.goto("https://akash-parida-iiit-bh.neetoplanner.net/");
+        await page.locator('[data-test-id="neeto-auth-email-input-field"]').fill("cpts9gnqty9-planner-akash_parida-iiit_bh@bigbinary.com");
+        await page.locator('[data-test-id="neeto-auth-login-button"]').click();
+        await page.getByPlaceholder('Enter 6 digit login code').fill(loginCode);
+        await expect(page.locator('.neeto-molecules-sidebar__logo')).toBeVisible(({
+            timeout: 300000,
+        }));
+    })
+})
