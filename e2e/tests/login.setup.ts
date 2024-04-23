de676d3c367ded5fb49fcacef11d2bcd762282de
akash5k <akashparida185@gmail.com>
2024-04-23 15:26:29 +0530

    remove redundant awaits

--- e2e/tests/login.setup.ts
+++ e2e/tests/login.setup.ts
@@ -1,6 +1,6 @@
 //login.spec.ts
 
-import { Faker, faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker"
 import { test } from "../fixtures"
 import { EMAIL } from "../constants/texts/login";
 import { STORAGE_STATE } from "../../playwright.config";
@@ -11,9 +11,10 @@ test.describe("Login page", () => {
         loginCode = faker.string.numeric(6);
     })
     test("should login with the correct credentials", async ({ page, loginPage }) => {
-        await test.step("Step 1 : Navigate to base url", async () => { 
-            await page.goto("https://akash-parida-iiit-bh.neetoplanner.net/"); 
-        })
+        await test.step("Step 1 : Navigate to base url", ()=>{
+             page.goto("https://akash-parida-iiit-bh.neetoplanner.net/")
+            })
+        
 
         await test.step ("Step 2 : Login to the application",async()=>{
             await loginPage.loginAndVerify({
