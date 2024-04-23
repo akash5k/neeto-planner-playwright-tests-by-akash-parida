b68f1888736c0336a48b66cdb1277377a7325ebe
akash5k <akashparida185@gmail.com>
2024-04-23 11:41:59 +0530

    fix wrong login.ts

--- e2e/poms/login.ts
+++ e2e/poms/login.ts
@@ -1,25 +1,21 @@
-//login.spec.ts
+// poms/login.ts
 
-import { Faker, faker } from "@faker-js/faker"
-import { test } from "../fixtures"
-import { STORAGE_STATE } from "../../playwright.config";
+import { Page, expect } from "@playwright/test";
+import { BUTTON_SELECTORS ,INPUT_SELECTORS} from "../constants/selectors/login";
 
-test.describe("Login page", () => {
-    let loginCode: string;
-    test.beforeEach(() => {
-        loginCode = faker.string.numeric(6);
-    })
-    test("should login with the correct credentials", async ({ page, loginPage }) => {
-        await test.step("Step 1 : Navigate to base url", async () => { 
-            await page.goto("https://akash-parida-iiit-bh.neetoplanner.net/"); 
-        })
+export default class LoginPage {
+    page: Page;
+    constructor(page: Page){
+        this.page =page;
+    }
 
-        await test.step ("Step 2 : Login to the application",async()=>{
-            await loginPage.loginAndVerify({
-                email: "cpts9gnqty9-planner-akash_parida-iiit_bh@bigbinary.com",
-                loginCode: loginCode
-            })
-            await page.context().storageState({ path: STORAGE_STATE });
-        })
-    })
-})
+    loginAndVerify = async ({email,loginCode}:{email:string,loginCode:string}):Promise<void>=>{
+        await this.page.getByTestId(INPUT_SELECTORS.emailInput).fill(email)
+        await this.page.getByTestId(BUTTON_SELECTORS.loginButton).click();        
+        await this.page.getByPlaceholder(INPUT_SELECTORS.loginCode).fill(loginCode);
+        //verify if logged in successful
+        await expect(this.page.getByTestId('main-header')).toBeVisible(({
+            timeout:50000,
+        }))
+    }
+}
