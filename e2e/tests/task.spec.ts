85557342f2bd676058116a2624beb6b24f253367
akash5k <akashparida185@gmail.com>
2024-04-27 23:13:32 +0530

    add loops for task test

--- e2e/tests/task.spec.ts
+++ e2e/tests/task.spec.ts
@@ -1,28 +1,19 @@
 import { expect } from "@playwright/test";
-import { faker } from "@faker-js/faker";
+import { faker, fi } from "@faker-js/faker";
 import { test } from "../fixtures/index";
+import { COMMON_SELECTORS, COMMON_TEXTS, TEST_DATA } from "../constants/common";
 
 test.describe("Create and verify tasks", () => {
-    let firstProjectName: string;
-    let firstTaskName: string;
-    let firstTaskDescription: string;
-    let firstTaskComment: string;
-
-    let secondProjectName: string;
-    let secondTaskName: string;
-    let secondTaskDescription: string;
-    let secondTaskComment: string;
+    let projectNames: string[];
+    let taskNames: string[];
+    let taskDescriptions: string[];
+    let taskComments: string[];
 
     test.beforeEach(() => {
-        firstProjectName = faker.word.words({ count: 3 });
-        firstTaskName = faker.word.words({ count: 2 });
-        firstTaskDescription = faker.word.words({ count: 5 });
-        firstTaskComment = faker.word.words({ count: 5 });
-
-        secondProjectName = faker.word.words({ count: 3 });
-        secondTaskName = faker.word.words({ count: 2 });
-        secondTaskDescription = faker.word.words({ count: 5 });
-        secondTaskComment = faker.word.words({ count: 5 });
+        projectNames = [faker.word.words({ count: 3 }), faker.word.words({ count: 3 })];
+        taskNames = [faker.word.words({ count: 2 }), faker.word.words({ count: 2 })];
+        taskDescriptions = [faker.word.words({ count: 5 }), faker.word.words({ count: 5 })];
+        taskComments = [faker.word.words({ count: 5 }), faker.word.words({ count: 5 })];
     });
 
     test("should create and verify tasks", async ({ page, projectPage, taskPage }) => {
@@ -30,53 +21,40 @@ test.describe("Create and verify tasks", () => {
             await page.goto("/");
         });
 
-        await test.step("Step 2: Assert there are no assigned tasks currently", async () => {
-            await page.locator('[data-testid="navlink-tasks"]').click();
+        await test.step("Step 2: Assert there are no assigned tasks", async () => {
+            await page.locator(COMMON_TEXTS.tasksNav).click();
             await expect(page.getByRole('heading', { name: '0 tasks' })).toBeVisible();
-            await page.locator('[data-testid="navlink-projects"]').click();
+            await page.locator(COMMON_TEXTS.projectsNav).click();
         });
 
-        await test.step("Step 3: Create first project and task", async () => {
-            await projectPage.addProject({ projectName: firstProjectName });
-            await taskPage.addTask({ taskName: firstTaskName, taskAssignee: 'Akash Parida' });
-            await taskPage.addDescriptionAndComment({
-                taskName: firstTaskName,
-                taskDescription: firstTaskDescription,
-                taskComment: firstTaskComment
-            });
-            //navigate to base url for locator to find add project button
-            await page.goto("/")
-        });
-        await test.step("Step 4: Create second project and task", async () => {
-            await projectPage.addProject({ projectName: secondProjectName });
-            await taskPage.addTask({ taskName: secondTaskName, taskAssignee: 'Akash Parida' });
-            await taskPage.addDescriptionAndComment({
-                taskName: secondTaskName,
-                taskDescription: secondTaskDescription,
-                taskComment: secondTaskComment
-            });
+        await test.step("Step 3: Create projects and tasks", async () => {
+            for (let i = 0; i < projectNames.length; i++) {
+                await projectPage.addProject({ projectName: projectNames[i] });
+                await taskPage.addTask({ taskName: taskNames[i], taskAssignee: TEST_DATA.userName });
+                await taskPage.addDescriptionAndComment({
+                    taskName: taskNames[i],
+                    taskDescription: taskDescriptions[i],
+                    taskComment: taskComments[i]
+                });
+                await page.goto("/");
+            }
         });
 
-        await test.step("Step 5: Verify tasks in Tasks section", async () => {
-            await page.locator('[data-testid="navlink-tasks"]').click();
-            await taskPage.verifyDescriptionAndComment({
-                taskName: firstTaskName,
-                taskDescription: firstTaskDescription,
-                taskComment: firstTaskComment,
-                taskAssignee: 'Akash Parida'
-            });
-            await page.locator('[data-cy="pane-close-button"]').click()
-            await taskPage.verifyDescriptionAndComment({
-                taskName: secondTaskName,
-                taskDescription: secondTaskDescription,
-                taskComment: secondTaskComment,
-                taskAssignee: 'Akash Parida'
-            });
-        });
-
-        await test.step("Step 6: Delete the created projects", async () => {
-            await projectPage.deleteProject({ projectName: firstProjectName });
-            await projectPage.deleteProject({ projectName: secondProjectName });
+        await test.step("Step 4: Verify tasks in Tasks section", async () => {
+            await page.locator(COMMON_TEXTS.tasksNav).click();
+            for (let i = 0; i < taskNames.length; i++) {
+                await taskPage.verifyDescriptionAndComment({
+                    taskName: taskNames[i],
+                    taskDescription: taskDescriptions[i],
+                    taskComment: taskComments[i],
+                    taskAssignee: TEST_DATA.userName
+                });
+            }
         });
     });
+    test.afterEach(async ({ projectPage }) => {
+        for (let projectName of projectNames) {
+            await projectPage.deleteProject({ projectName });
+        }
+    })
 });
